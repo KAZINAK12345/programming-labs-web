b1880bc5f65642edc535f23d1bb4f0063eb549b1
KAZINAK12345 <aptdoll@yandex.ru>
2023-06-05 19:44:54 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -15,3 +15,12 @@ function showDate() {
     out4.innerHTML = today4.toLocaleString('ja-JP');
     out5.innerHTML = today5.toLocaleString('sv-SE');
 }
+function showDaysCount() {
+    let out = document.getElementById('Days');
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    daysCount = Math.floor(daysCount);
+    out.innerHTML = 'Количество дней с дня рождения ' + daysCount;
+}
